104ef9943a02e1f1ece781d9191cf0a7bb8867c7
RADCOLOUR <124600558+RADCOLOUR@users.noreply.github.com>
2026-03-23 01:43:06 +0000

    All v1.1.2 Files.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         minSdk = 30
         targetSdk = 36
         versionCode = 1
-        versionName = "1.1.1"
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
